1e15b85d9a2046e264359ab61d09a377a5e7f9a9
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-15 04:27:10 +0530

    Update: changes in Home.jsx in Pages

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const FormField = () => {
   return (
@@ -6,4 +6,4 @@ const FormField = () => {
   )
 }
 
-export default FormField
+export default FormField;
